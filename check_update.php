338e5bd749d332a3dbe191dcf122fd8944e24b2b
yd4irs-code <yd4irs@gmail.com>
2026-06-05 10:40:25 +0700

    fix: tambahkan tanggal di check_update.php agar Display TV otomatis reload saat pergantian hari (tengah malam)

--- check_update.php
+++ check_update.php
@@ -3,11 +3,14 @@ header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
 
+date_default_timezone_set('Asia/Jakarta');
 $file = 'db/last_updated.txt';
+$current_date = date('Y-m-d');
+
 if (file_exists($file)) {
-    echo trim(file_get_contents($file));
+    echo trim(file_get_contents($file)) . "_" . $current_date;
 } else {
     $time = (string)time();
     @file_put_contents($file, $time);
-    echo $time;
+    echo $time . "_" . $current_date;
 }
